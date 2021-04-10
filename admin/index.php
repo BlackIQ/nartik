75f14a0087ffd03790cc0897d5c9ba514b550c0c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-11 01:36:14 +0430

    Fix ip

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,18 @@
 <?php
 
-$ip = "127.0.0.1";
+$server = "localhost";
+$user = "narbon";
+$passwd = "narbon";
+$db = "nartik";
+
+$conn = mysqli_connect($server, $user, $passwd, $db);
+
+$getip = "SELECT * FROM development";
+$res = mysqli_query($conn, $getip);
+
+while ($row = mysqli_fetch_assoc($res)) {
+    $ip = $row['ip'];
+}
 
 if ($_SESSION['person'] == "admin") {
     
