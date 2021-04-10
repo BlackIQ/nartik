3eccd870b7ba980d2e429ee7d98d0e96d336e5a9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-11 02:15:12 +0430

    Still sessions

--- dashboard/index.php
+++ dashboard/index.php
@@ -16,8 +16,20 @@ while ($row = mysqli_fetch_assoc($res)) {
     $ip = $row['ip'];
 }
 
+$email = $_SESSION['email'];
+
 if ($_SESSION['status'] == true) {
-    echo $_SESSION['email'];
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($row['phone'] == "09366171566") {
+                $_SESSION['person'] = "admin";
+            }
+                header("Location: http://$ip/NarTik/admin");
+        }
+    }
 }
 else {
     header("Location: http://$ip/NarTik/account");
