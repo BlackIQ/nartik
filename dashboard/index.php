18c0d114bdea0cedcd9d0ac5bdfd1b25abf34d24
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-11 01:07:57 +0430

    Use email insteade of usernam

--- dashboard/index.php
+++ dashboard/index.php
@@ -5,7 +5,7 @@ session_start();
 $ip = "127.0.0.1";
 
 if ($_SESSION['status'] == true) {
-    echo $_SESSION['username'];
+    echo $_SESSION['email'];
 }
 else {
     header("Location: http://$ip/NarTik/account");
