c63afc5a33320cefc61977bc1acc8c627b6c37ea
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-10 23:46:49 +0430

    Going to work on login

--- account/action.php
+++ account/action.php
@@ -52,11 +52,11 @@
 
     // LOGIN USER
     if (isset($_POST['login_user'])) {
-        $username = mysqli_real_escape_string($connection, $_POST['username']);
+        $email = mysqli_real_escape_string($connection, $_POST['username']);
         $password = mysqli_real_escape_string($connection, $_POST['password']);
 
-        if (empty($username)) {
-            array_push($errors, "Username is required");
+        if (empty($email)) {
+            array_push($errors, "Email is required");
         }
         if (empty($password)) {
             array_push($errors, "Password is required");
@@ -64,7 +64,7 @@
 
         if (count($errors) == 0) {
             $password = md5($password);
-            $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+            $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
             $results = mysqli_query($connection, $query);
 
             if (mysqli_num_rows($results) == 1) {
