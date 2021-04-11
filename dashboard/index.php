6ddb97ed0a1e47a477683cf3bf20ba0524ea95d8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-12 00:47:31 +0430

    Fix welcome message and headers

--- dashboard/index.php
+++ dashboard/index.php
@@ -19,20 +19,19 @@ while ($row = mysqli_fetch_assoc($res)) {
 $email = $_SESSION['email'];
 
 if ($_SESSION['status'] == true) {
-    $sql = "SELECT * FROM users WHERE email = '$email'";
-    $result = mysqli_query($conn, $sql);
     
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            if ($row['phone'] == "09366171566") {
-                $_SESSION['person'] = "admin";
-            }
-                header("Location: http://$ip/NarTik/admin");
-        }
-    }
 }
 else {
     header("Location: http://$ip/NarTik/account");
+}
+
+$sql = "SELECT * FROM users WHERE email = '$email'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Welcome, " . $row['firstname'];
+    }
 }
 
 ?>
