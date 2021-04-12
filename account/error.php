494255c2df145da103c25d71456b5f1c60c1356e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-12 11:12:53 +0430

    H4

--- account/error.php
+++ account/error.php
@@ -1,7 +1,7 @@
 <?php  if (count($errors) > 0) : ?>
     <div class="alert alert-danger text-center" role="alert">
         <?php foreach ($errors as $error) : ?>
-            <h3><?php echo $error ?></h3>
+            <h4><?php echo $error ?></h4>
         <?php endforeach ?>
     </div>
     <br>
