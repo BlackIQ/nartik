0943cb0cd96188b6cab4a3c8bee7377d4182e809
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-12 11:10:33 +0430

    add br and text-center class

--- account/error.php
+++ account/error.php
@@ -1,7 +1,8 @@
 <?php  if (count($errors) > 0) : ?>
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger text-center" role="alert">
         <?php foreach ($errors as $error) : ?>
             <p><?php echo $error ?></p>
         <?php endforeach ?>
     </div>
+    <br>
 <?php  endif ?>
