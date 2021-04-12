64b8e6b74fc945220fe528f92780a480de707f7f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-12 11:06:03 +0430

    Center alart

--- account/error.php
+++ account/error.php
@@ -1,5 +1,5 @@
 <?php  if (count($errors) > 0) : ?>
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger text-center" role="alert">
         <?php foreach ($errors as $error) : ?>
             <p><?php echo $error ?></p>
         <?php endforeach ?>
