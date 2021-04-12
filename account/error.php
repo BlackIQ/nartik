91b61f28343089afb2c37f72f06c34afcfef87fc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-12 11:15:50 +0430

    Close

--- account/error.php
+++ account/error.php
@@ -2,6 +2,7 @@
     <div class="alert alert-danger text-center" role="alert">
         <?php foreach ($errors as $error) : ?>
             <h4><?php echo $error ?></h4>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">&times;</button>
         <?php endforeach ?>
     </div>
     <br>
