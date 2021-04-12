a74e66639e3144362154868ddfd123eea41a3788
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-12 11:08:19 +0430

    Toast

--- account/error.php
+++ account/error.php
@@ -1,7 +1,13 @@
 <?php  if (count($errors) > 0) : ?>
-    <div class="alert alert-danger text-center" role="alert">
-        <?php foreach ($errors as $error) : ?>
-            <p><?php echo $error ?></p>
-        <?php endforeach ?>
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <strong class="me-auto text-danger">There is a problem</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            <?php foreach ($errors as $error) : ?>
+                <p><?php echo $error ?></p>
+            <?php endforeach ?>
+        </div>
     </div>
 <?php  endif ?>
