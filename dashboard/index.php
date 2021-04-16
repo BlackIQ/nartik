a0447980a1aa39543fde78e14467789e499f5e2e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-16 13:33:21 +0430

    Use panels in a dir

--- dashboard/index.php
+++ dashboard/index.php
@@ -43,9 +43,46 @@ include("panels/sidebar.php");
     </div><!--/.row-->
 
     <div class="panel panel-container">
-        <?php
-        include("panels/bar.php");
-        ?>
+        
+        <div class="row">
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-teal panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-bank color-blue"></em>
+                        <div class="large"><?php echo "Milad"; ?></div>
+                        <br>
+                        <div class="text-muted">Company</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-blue panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-envelope color-orange"></em>
+                        <div class="large"><?php echo "8"; ?></div>
+                        <br>
+                        <div class="text-muted">Total Tickets</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-orange panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-check text-success"></em>
+                        <div class="large"><?php echo "7"; ?></div>
+                        <br>
+                        <div class="text-muted">Answered Tickets</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-red panel-widget ">
+                    <div class="row no-padding"><em class="fa fa-xl fa-times color-red"></em>
+                        <div class="large"><?php echo "1"; ?></div>
+                        <br>
+                        <div class="text-muted">Not Seen Tickets</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
     <div class="row">
         <div class="col-md-6">
@@ -238,9 +275,23 @@ include("panels/sidebar.php");
         </div>
     </div><!--/.row-->
 
-    <?php
-    include("panels/footer.php");
-    ?>
+    <div class="row">
+        <div class="col-sm-12">
+            <p class="back-link"><a href="http://<?php echo $ip; ?>/Narbon">NarTik</a></p>
+            <p class="back-link">Created by <a href="https://www.github.com/BlackIQ">Amirhossein Mohammadi</a></p>
+            <p class="back-link">Powered By <a href="https://www.linkedin.com/company/neotrinost">Neotrinost</a> <i class="fa fa-copyright"></i> <?php echo date("Y"); ?></p>
+            <p class="back-link">
+                <i class="fa fa-lg fa-linkedin-square text-info"></i>
+                &nbsp;
+                <i class="fa fa-lg fa-github"></i>
+                &nbsp;
+                <i class="fa fa-lg fa-telegram text-primary"></i>
+                &nbsp;
+                <i class="fa fa-lg fa-instagram text-danger"></i>
+            </p>
+        </div>
+    </div>
+    
 </div>    <!--/.main-->
 
 <script src="js/jquery-1.11.1.min.js"></script>
