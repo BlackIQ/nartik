2d55fa18c6112bfc3ef809b5ff2ff6a63343a0e7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-17 15:28:42 +0430

    Fix url

--- dashboard/index.php
+++ dashboard/index.php
@@ -28,8 +28,8 @@ if ($_SESSION['status'] == true) {
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i"
           rel="stylesheet">
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
+    <script src="pack/js/html5shiv.js"></script>
+    <script src="pack/js/respond.min.js"></script>
     <![endif]-->
 </head>
 <body>
