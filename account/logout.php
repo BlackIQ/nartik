c20279621aa153300db66deec0e2caa53e33169f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-18 11:10:07 +0430

    Fix logout

--- account/logout.php
+++ account/logout.php
@@ -2,20 +2,6 @@
 
 session_start();
 
-$server = "localhost";
-$user = "narbon";
-$passwd = "narbon";
-$db = "nartik";
-
-$conn = mysqli_connect($server, $user, $passwd, $db);
-
-$getip = "SELECT * FROM development";
-$res = mysqli_query($conn, $getip);
-
-while ($row = mysqli_fetch_assoc($res)) {
-    $ip = $row['ip'];
-}
-
 session_destroy();
 
-header("Location: http://$ip/NarTik");
+header("Location: http://office.narbon.ir/NarTik");
