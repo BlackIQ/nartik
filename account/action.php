2c315f2219ad6310e65a978e54715a459b9baea0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-28 17:44:41 +0430

    Fix login

--- account/action.php
+++ account/action.php
@@ -66,13 +66,13 @@
 
         if (count($errors) == 0) {
 //            $password = md5($password);
-            $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+            $query = "SELECT * FROM people WHERE type='user' AND email='$email' AND password='$password'";
             $results = mysqli_query($connection, $query);
 
             if (mysqli_num_rows($results) == 1) {
                 $_SESSION['status'] = true;
-                $_SESSION['email'] = $email;
-                $_SESSION['success'] = "You are now logged in";
+                $_SESSION['id'] = $id;
+                $_SESSION['directory'] = 'nartik';
                 header('location: http://office.narbon.ir:4488/NarTik');
             }
             else {
