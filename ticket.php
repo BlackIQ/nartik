48f3b331a5677e71fcfe3ca5fc7bc9823156522f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-04 23:10:11 +0430

    Add answer part

--- ticket.php
+++ ticket.php
@@ -13,7 +13,18 @@ if (count($tik) > 0) {
             <h3><?php echo $tik[0]['explane']; ?></h3>
             <br>
             <p>Sent in : <?php echo $tik[0]['dt']; ?></p>
-            <hr>
+            <br>
+            <h3>Answer</h3>
+            <p>
+                <?php
+                    if ($tik[0]['answer'] == 'ny') {
+                        echo 'No answers yet';
+                    }
+                    else {
+                        echo $tik[0]['answer'];
+                    }
+                ?>
+            </p>
         </div>
         <?php
     }
