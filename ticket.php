db6ed1a56e38ebe6ad36db8bd41119dee21ae9c9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-04 23:08:01 +0430

    Remove some lines

--- ticket.php
+++ ticket.php
@@ -7,13 +7,10 @@ if (count($tik) > 0) {
     else {
         ?>
         <div>
-            <h2><?php echo $tik[0]['tikid']; ?></h2>
+            <b><?php echo $tik[0]['tikid']; ?></b>
             <hr>
             <h3><b><?php echo $tik[0]['title']; ?></b></h3>
             <h3><?php echo $tik[0]['explane']; ?></h3>
-            <br>
-            <h4>Company : <?php echo $tik[0]['company']; ?></h4>
-            <h4>User : <?php echo $tik[0]['userid']; ?></h4>
             <br>
             <p>Sent in : <?php echo $tik[0]['dt']; ?></p>
             <hr>
