a183d216572750d7245250b0e3dfb862f17cec6c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-06 20:26:33 +0430

    Fixed send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php  if (count($send) > 0) : ?>
-    <div class="alert alert-danger text-center" role="alert">
+    <div class="alert alert-info text-center" role="alert">
         <?php foreach ($send as $error) : ?>
             <h4><?php echo $error ?></h4>
         <?php endforeach ?>
