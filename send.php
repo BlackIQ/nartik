a045d316787e2082b7e7ba37a331c5eeaa61255a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-06 20:32:58 +0430

    Test new thing :)

--- send.php
+++ send.php
@@ -1,8 +1,17 @@
-<?php  if (count($send) > 0) : ?>
+<?php
+
+if (count($send) > 0) {
+    ?>
     <div class="alert alert-info text-center" role="alert">
-        <?php foreach ($send as $error) : ?>
-            <h4><?php echo $error ?></h4>
-        <?php endforeach ?>
+        <?php
+            foreach ($send as $error) {
+                ?>
+                    <h4><?php echo $error ?></h4>
+                <?php
+            }
+        ?>
     </div>
-    <br>
-<?php  endif ?>
+    <?php
+}
+
+?>
