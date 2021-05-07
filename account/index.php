c1822f7c2bb40eb6ae9e395036f25d256873a471
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-07 16:41:54 +0430

    Fixed something

--- account/index.php
+++ account/index.php
@@ -85,11 +85,11 @@ if ($_SESSION['status'] == true) {
                         <div class="row">
                             <div class="col">
                                 <select name="company" class="form-control border-white">
-                                    <option velue="none" selected>انتخاب شرکت</option>
+                                    <option selected>انتخاب شرکت</option>
                                     <option value="Narbon">ناربن</option>
                                     <option value="Milad">میلاد</option>
                                     <option value="Microsoft">مایکروسافت</option>
-                                    <option value="Apple</option>
+                                    <option value="Apple">اپل</option>
                                 </select>
                             </div>
                         </div>
