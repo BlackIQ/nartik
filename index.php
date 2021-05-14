1cf8f90b86edcc1709457af3c611d223ed958032
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-14 16:56:38 +0430

    Fix modal id

--- index.php
+++ index.php
@@ -412,7 +412,7 @@ else {
 </html>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="confirmlogout" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
