c32f1b10cc6514317d77995a215b28d630b8607f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-14 16:17:56 +0430

    Fix query

--- account/action.php
+++ account/action.php
@@ -96,7 +96,7 @@
 
         if (count($errors) == 0) {
 //            $password = md5($password);
-            $query = "SELECT * FROM people WHERE type='user' AND email='$email' AND password='$password'";
+            $query = "SELECT * FROM people WHERE type='user' AND id='$id' AND password='$password'";
             $results = mysqli_query($connection, $query);
 
             if (mysqli_num_rows($results) == 1) {
