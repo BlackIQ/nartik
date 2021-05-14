2cf3a7ad10791b6697f25c013597c35f2746b17c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-14 19:22:51 +0430

    Move modal in center

--- index.php
+++ index.php
@@ -413,7 +413,7 @@ else {
 
 <!-- Modal -->
 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-sm" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 style="float: left;" class="modal-title text-danger" id="exampleModalLongTitle">خروج از حساب</h5>
