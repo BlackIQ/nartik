f1c4ee71e92efc81038df54571235db3ad3a83d7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-15 09:47:08 +0430

    Creating site close

--- index.php
+++ index.php
@@ -11,30 +11,41 @@ $mysqldatabase = "nartik";
 
 $connection = mysqli_connect($mysqlserver, $mysqluser, $mysqlpassword, $mysqldatabase);
 
-if ($_SESSION['status'] == true) {
-    $id = $_SESSION['id'];
+date_default_timezone_set('Iran');
+    
+$hour = date("H");
 
-    $getdata = "SELECT * FROM people WHERE type='user' AND id='$id'";
-    $ressult = mysqli_query($connection, $getdata);
-
-    if (mysqli_num_rows($ressult) > 0) {
-        while ($row = mysqli_fetch_assoc($ressult)) {
-            $fname = $row['firstname'];
-            $lname = $row['lastname'];
-            $phone = $row['phone'];
-            $email = $row['email'];
-            $username = $row['username'];
-            $userid = $row['id'];
-            $company = $row['company'];
-        }
-    }
-
-    $gettiks = "SELECT * FROM tiks WHERE userid = '$userid' ORDER BY row DESC";
-    $tikres = mysqli_query($connection, $gettiks);
+if (($hour <= 24 && $hour >= 9)) {
+    echo "متاسفانه بسته هستیم";
 }
 else {
-    header("Location: http://office.narbon.ir:4488/NarTik/account");
+    if ($_SESSION['status'] == true) {
+        $id = $_SESSION['id'];
+
+        $getdata = "SELECT * FROM people WHERE type='user' AND id='$id'";
+        $ressult = mysqli_query($connection, $getdata);
+
+        if (mysqli_num_rows($ressult) > 0) {
+            while ($row = mysqli_fetch_assoc($ressult)) {
+                $fname = $row['firstname'];
+                $lname = $row['lastname'];
+                $phone = $row['phone'];
+                $email = $row['email'];
+                $username = $row['username'];
+                $userid = $row['id'];
+                $company = $row['company'];
+            }
+        }
+
+        $gettiks = "SELECT * FROM tiks WHERE userid = '$userid' ORDER BY row DESC";
+        $tikres = mysqli_query($connection, $gettiks);
+    }
+    else {
+        header("Location: http://office.narbon.ir:4488/NarTik/account");
+    }
 }
+
+
 
 
 ?>
