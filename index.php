6906ec50072367b2cd92e278d8bd842a18ac672e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-15 15:22:44 +0430

    Fixing work time

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ date_default_timezone_set('Iran');
 $hour = date("H");
 
 if (hour <= 24 && $hour >= 6) {
-    include("close.php");
+    include("main.php");
 }
 else {
     echo "Close";
     if ($_SESSION['status'] == true) {
-        include("main.php");
+        include("close.php");
     }
     else {
         header("Location: http://office.narbon.ir:4488/NarTik/account");
