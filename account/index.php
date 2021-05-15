803d99ec4a3f98e5e2c380a674e883679317d85d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-15 15:16:15 +0430

    Working on shadow v3

--- account/index.php
+++ account/index.php
@@ -38,14 +38,15 @@ if ($_SESSION['status'] == true) {
                 padding: 20px;
                 border-bottom-left-radius: 0px;
                 border-top-right-radius: 0px;
-                box-shadow:
+/*                box-shadow:
                     0 2.8px 2.2px rgba(0, 0, 0, 0.034),
                     0 6.7px 5.3px rgba(0, 0, 0, 0.048),
                     0 12.5px 10px rgba(0, 0, 0, 0.06),
                     0 22.3px 17.9px rgba(0, 0, 0, 0.072),
                     0 41.8px 33.4px rgba(0, 0, 0, 0.086),
                     0 100px 80px rgba(0, 0, 0, 0.12)
-                ;
+                ;*/
+                box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
             }
             .inp {
                 border: solid 1px white;
