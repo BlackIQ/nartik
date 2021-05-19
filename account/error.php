3af0cb76381e53b12b74f8c8eed19171d0166431
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-20 00:46:14 +0430

    New alerts design

--- account/error.php
+++ account/error.php
@@ -1,7 +1,7 @@
 <?php  if (count($errors) > 0) : ?>
-    <div class="alrt">
+    <div class="alert alert-danger alert-dismissible fade show mform border-danger">
         <?php foreach ($errors as $error) : ?>
-            <h5><?php echo $error ?></h5>
+            <h5 style="text-white"><?php echo $error ?></h5>
         <?php endforeach ?>
     </div>
     <br>
