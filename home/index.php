7c39b9ca7e0908fa5847019e361f941b154607a3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-21 00:25:58 +0430

    A home for rent and information

---
+++ home/index.php
@@ -0,0 +1,18 @@
+<?php
+
+if (hour <= 24 && $hour >= 6) {
+    if ($_SESSION['status'] == true) {
+        if ($_SESSION["who"] == "user") {
+            header("Location: http://127.0.0.1/NarTik/user");
+        }
+        elseif ($_SESSION["who"] == "support") {
+            header("Location: http://127.0.0.1/NarTik/support");
+        }
+    }
+    else {
+        header("Location: http://127.0.0.1/NarTik/home");
+    }
+}
+else {
+    include("../etc/close.php");
+}
