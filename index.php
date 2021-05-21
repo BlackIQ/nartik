182a40fa9a0cfe72232f1734d8fe5f74efc8000f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-21 22:53:36 +0430

    Working on index front-end

--- index.php
+++ index.php
@@ -6,6 +6,57 @@ if ($_SESSION['status'] == true) {
     $who = $_SESSION['who'];
     header("Location : http://127.0.0.1/NarTik/people/$who");
 }
-else {
-    echo 'Login first';
-}
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <style>
+            @font-face {
+                font-family: 'nazanin';
+                src: url('http://127.0.0.1/NarTik/pack/nazanin.TTF');
+                font-style: normal;
+            }
+
+            body {
+                background: #f1f1f1;
+                text-align: center;
+                direction: rtl;
+            }
+            
+            * {
+                font-family: 'nazanin';
+            }
+            
+            .org {
+                margin-top: 15%;
+            }
+        </style>
+        <title>نارتیک</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="http://127.0.0.1/NarTik/pack/css/bootstrap.min.css" rel="stylesheet">
+        <link href="http://127.0.0.1/NarTik/pack/css/datepicker3.css" rel="stylesheet">
+        <link href="http://127.0.0.1/NarTik/pack/css/styles.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="col-md-4">
+                <br>
+            </div>
+            <div class="col-md-4 org">
+                <h
+                <button class="btn btn-lg btn-primary"><b>ورود کاربر</b></button>
+                &nbsp;
+                <button class="btn btn-lg btn-success"><b>ورود پشتیبان</b></button>
+            </div>
+            <div class="col-md-4">
+                <br>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    </body>
+</html>
