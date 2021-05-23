900d62564620f4938212dc8f9955a3199f1e7723
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-24 00:40:36 +0430

    Remove useless fields from admin table

--- pack/sql.sql
+++ pack/sql.sql
@@ -3,10 +3,6 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `row` int(11) NOT NULL AUTO_INCREMENT,
   `id` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `firstname` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `lastname` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `company` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `uid` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -14,8 +10,8 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 LOCK TABLES `admin` WRITE;
-INSERT INTO `admin` VALUES (1, '0482222222','Mehrdad','Mirzaee','09366171566','mehrdad@gmail.com','mrmehrdad', 'Milad', 10);
-INSERT INTO `admin` VALUES (2, '0481111111','Meysam','Mirzaee','09123432821','meysam@gmail.com','mrmeysam', 'Narbon', 20);
+INSERT INTO `admin` VALUES (1, 'milad', 'milad', 'Milad', 10);
+INSERT INTO `admin` VALUES (2, 'narbon', 'narbon', 'Narbon', 20);
 UNLOCK TABLES;
 
 -- People
