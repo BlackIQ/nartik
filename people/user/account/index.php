91324a5184b00d2d428306aca7829f7887c699ff
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-02 23:32:25 +0430

    Update config

--- people/user/account/index.php
+++ people/user/account/index.php
@@ -10,10 +10,11 @@ if ($_SESSION['status'] == true) {
 include('action.php');
 
 // MySQL Data
-$mysqlserver = "localhost";
-$mysqluser = "narbon";
-$mysqlpassword = "narbon";
-$mysqldatabase = "nartik";
+include("../../../pack/config.php");
+$mysqlserver = $serverip;
+$mysqluser = $dbuser;
+$mysqlpassword = $dbpassword;
+$mysqldatabase = $dbname;
 
 // Create Connection
 $connection = mysqli_connect($mysqlserver, $mysqluser, $mysqlpassword, $mysqldatabase);
