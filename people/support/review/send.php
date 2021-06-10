e0692a62a10bd04d60a9942759da31fa860d8be8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-11 00:45:30 +0430

    Fix color

--- people/support/review/send.php
+++ people/support/review/send.php
@@ -2,7 +2,7 @@
 
 if (count($send) > 0) {
     ?>
-    <div class="alert alert-info text-center" role="alert">
+    <div class="alert alert-success text-center" role="alert">
         <?php
         foreach ($send as $error) {
             echo '<h4>' . $error . '</h4>';
