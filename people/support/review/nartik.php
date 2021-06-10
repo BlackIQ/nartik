48ac0a2f7f2f221687ccf7720e3ef14bb5671516
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-11 00:22:40 +0430

    Change array name

--- people/support/review/nartik.php
+++ people/support/review/nartik.php
@@ -1,27 +1,27 @@
 <?php
 
-if (count($tik) > 0) {
-    if ($tik[0] == false) {
+if (count($com_tik) > 0) {
+    if ($com_tik[0] == false) {
         echo '<h2>تیکت پیدا نشد</h2>';
     }
     else {
         ?>
         <div>
-            <b><?php echo $tik[0]['tikid']; ?></b>
+            <b><?php echo $com_tik[0]['tikid']; ?></b>
             <hr>
-            <h3><b><?php echo $tik[0]['title']; ?></b></h3>
-            <h3><?php echo $tik[0]['explane']; ?></h3>
+            <h3><b><?php echo $com_tik[0]['title']; ?></b></h3>
+            <h3><?php echo $com_tik[0]['explane']; ?></h3>
             <br>
-            <p><?php echo $tik[0]['dt'] . "&nbsp;"; ?>ارسال شده در</p>
+            <p><?php echo $com_tik[0]['dt'] . "&nbsp;"; ?>ارسال شده در</p>
             <hr>
             <h3>پاسخ</h3>
             <p>
                 <?php
-                if ($tik[0]['answer'] == 'ny') {
+                if ($com_tik[0]['answer'] == 'ny') {
                     echo 'هنوز به این تیکت پاسخی داده نشده است.<br>لطفا شکیبا باشید.';
                 }
                 else {
-                    echo $tik[0]['answer'];
+                    echo $com_tik[0]['answer'];
                 }
                 ?>
             </p>
