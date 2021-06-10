6b9dcfaf1340be801c3ca1a815b8c3b30d07e82c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-11 00:07:11 +0430

    Add a review part for review nartik ticket

---
+++ people/support/review/nartik.php
@@ -0,0 +1,38 @@
+<?php
+
+if (count($tik) > 0) {
+    if ($tik[0] == false) {
+        echo '<h2>تیکت پیدا نشد</h2>';
+    }
+    else {
+        ?>
+        <div>
+            <b><?php echo $tik[0]['tikid']; ?></b>
+            <hr>
+            <h3><b><?php echo $tik[0]['title']; ?></b></h3>
+            <h3><?php echo $tik[0]['explane']; ?></h3>
+            <br>
+            <p><?php echo $tik[0]['dt'] . "&nbsp;"; ?>ارسال شده در</p>
+            <hr>
+            <h3>پاسخ</h3>
+            <p>
+                <?php
+                if ($tik[0]['answer'] == 'ny') {
+                    echo 'هنوز به این تیکت پاسخی داده نشده است.<br>لطفا شکیبا باشید.';
+                }
+                else {
+                    echo $tik[0]['answer'];
+                }
+                ?>
+            </p>
+            <hr>
+            <button class="btn btn-defult"><a style="color: black;" href="index.php">بستن تیکت</a></button>
+        </div>
+        <?php
+    }
+}
+else {
+    echo '<h2>یک تیکت را انتخاب کنید</h2>';
+}
+
+?>
