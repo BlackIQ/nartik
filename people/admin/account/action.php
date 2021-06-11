ec5c5bd28028bf5e0858c7d61c3f8572a2805fb5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-11 12:13:53 +0430

    Fix action

---
+++ people/admin/account/action.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$errors = array();
+
+// MySQL Data
+include("../../../pack/config.php");
+$mysqlserver = "localhost";
+$mysqluser = $dbuser;
+$mysqlpassword = $dbpassword;
+$mysqldatabase = $dbname;
+
+// Create Connection
+$connection = mysqli_connect($mysqlserver, $mysqluser, $mysqlpassword, $mysqldatabase);
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+
+    if (empty($username)) {
+        array_push($errors, "نام کاربری الزامیست");
+    }
+    if (empty($password)) {
+        array_push($errors, "رمز الزامیست");
+    }
+
+    if (count($errors) == 0) {
+        if ($username == "admin" && $password == "admin") {
+            $_SESSION['status'] = true;
+            $_SESSION["who"] = "admin";
+            header("location: http://$serverip/NarTik/people/admin");
+        }
+        else {
+            array_push($errors, "نام کاربری یا رمز");
+        }
+    }
+}
+
+?>
