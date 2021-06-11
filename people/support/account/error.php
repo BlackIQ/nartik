c0dc49158a42c85ef49540a2135d17414c8a52dc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-11 17:46:38 +0430

    Update alert box style

--- people/support/account/error.php
+++ people/support/account/error.php
@@ -1,7 +1,7 @@
 <?php  if (count($errors) > 0) : ?>
-    <div class="alert alert-danger text-center" role="alert">
+    <div class="alert alert-danger border-danger" role="alert">
         <?php foreach ($errors as $error) : ?>
-            <h4><?php echo $error ?></h4>
+            <h4 style="color: red;"><?php echo $error ?></h4>
         <?php endforeach ?>
     </div>
     <br>
