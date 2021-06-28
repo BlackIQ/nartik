06e0a9ce9ab560adcca8ed73bc27296f10ef479f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-28 16:17:09 +0430

    Fix all links

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($_SESSION['status'] == true) {
         <style>
             @font-face {
                 font-family: 'nazanin';
-                src: url('http://<?php echo $serverip; ?>/NarTik/pack/fonts/nazanin.TTF');
+                src: url('pack/fonts/nazanin.TTF');
                 font-style: normal;
             }
 
@@ -43,9 +43,9 @@ if ($_SESSION['status'] == true) {
         <meta charset="utf-8">
         <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="http://<?php echo $serverip; ?>/NarTik/pack/css/bootstrap.min.css" rel="stylesheet">
-        <link href="http://<?php echo $serverip; ?>/NarTik/pack/css/datepicker3.css" rel="stylesheet">
-        <link href="http://<?php echo $serverip; ?>/NarTik/pack/css/styles.css" rel="stylesheet">
+        <link href="pack/css/bootstrap.min.css" rel="stylesheet">
+        <link href="pack/css/datepicker3.css" rel="stylesheet">
+        <link href="pack/css/styles.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     </head>
     <body>
@@ -83,5 +83,3 @@ if ($_SESSION['status'] == true) {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </body>
 </html>
-
-<!-- Start Modals -->
