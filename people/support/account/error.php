40fb38bac6bb5c9c0e8050dca36f6a21d17d6d53
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-30 09:15:44 +0430

    Remove error file for support

--- people/support/account/error.php
+++
@@ -1,8 +0,0 @@
-<?php  if (count($errors) > 0) : ?>
-    <div class="alert alert-danger border-danger" role="alert">
-        <?php foreach ($errors as $error) : ?>
-            <h4 style="color: red;"><?php echo $error ?></h4>
-        <?php endforeach ?>
-    </div>
-    <br>
-<?php  endif ?>
