234517d7ac16ba56fb529f40cefc7fa1d84e8bb4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-01 09:26:57 +0430

    testing something

--- people/admin/account/index.php
+++ people/admin/account/index.php
@@ -48,6 +48,18 @@ if (isset($_POST['login_user'])) {
     }
 }
 
+if (isset($_GET['logout'])) {
+    $logout = $_GET['logout'];
+    if ($logout == true) {
+        session_destroy();
+        ?>
+        <script>
+            window.location.replace("../../../")
+        </script>
+        <?php
+    }
+}
+
 ?>
 
 <!doctype html>
