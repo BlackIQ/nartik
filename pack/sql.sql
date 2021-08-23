498b2a7bd7fd911f996e1c529f6228f813088ab1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-23 18:22:39 +0430

    Remove useless tables

--- pack/sql.sql
+++ pack/sql.sql
@@ -34,20 +34,6 @@ CREATE TABLE `admin`
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admin`
---
-
-LOCK
-TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin`
-VALUES (1, 'milad', 'milad', 'Milad', '10'),
-       (2, 'narbon', 'narbon', 'Narbon', '20');
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-UNLOCK
-TABLES;
-
---
 -- Table structure for table `company`
 --
 
@@ -115,29 +101,3 @@ CREATE TABLE `tiks`
     PRIMARY KEY (`row`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
-DROP TABLE IF EXISTS `nartiks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `nartiks`
-(
-    `row`     int(11) NOT NULL AUTO_INCREMENT,
-    `tikid`   text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `title`   text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `explane` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `company` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `dt`      text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `answer`  text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    PRIMARY KEY (`row`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-06-02 23:57:14
