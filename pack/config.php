758445135db1933f0b66a95873eb46d54ffb6ad5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-23 13:14:58 +0430

    New config

--- pack/config.php
+++ pack/config.php
@@ -1,4 +1,13 @@
 <?php
 
-// IP
-$ip = "127.0.0.1";
+// Database config
+$server = "localhost";
+$user = "amir";
+$passwd = "amir";
+$db = "nartik";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
+
+// Site
+// $host = "https://nartik.net";
