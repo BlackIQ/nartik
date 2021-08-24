3e6bf1cb9fb56d732dc0f1d04e0a51ed95788026
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-24 10:46:18 +0430

    New style

--- index.php
+++ index.php
@@ -1,120 +1,85 @@
+<?php
+
+session_start();
+
+if ($_SESSION['status'] == true) {
+    $who = $_SESSION['who'];
+    ?>
+    <script>
+        window.location.replace("people/<?php echo $who; ?>");
+    </script>
+    <?php
+}
+
+?>
+
+<!doctype html>
 <html>
+    <head>
+        <style>
+            @font-face {
+                font-family: vazir;
+                src: url("pack/fonts/vazir/Vazir.ttf");
+            }
 
-<head>
-    <meta http-equiv="Content-Language" content="en-us">
-    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-    <title>نارتیک</title>
-    <meta name="samandehi" content="127099785"/>
-    <style>
-        .link {
-            text-decoration: none;
-            font-size: 12px;
-        }
-    </style>
-</head>
+            .body {
+                background: white;
+                text-align: center;
+                direction: rtl;
+            }
 
-<body>
+            * {
+                font-family: vazir, serif;
+            }
 
-<p>&nbsp;
-</p>
-<div align="center">
-    <table border="0" width="900">
-        <tr>
-            <td colspan="2">
-                <b><font face="Calibri" color="#585858" style="font-size: 14pt">
-                        NARBON </font>
-                    <font style="font-size: 14pt;" face="Trebuchet MS" color="#666666">
-                        technologies</font></b></td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <p align="center">
-                    <img border="0" src="http://narbon.ir/images/splash.jpg" width="900" height="300"></td>
-        </tr>
-        <tr>
-            <td width="253" colspan="2">
-                <p style="margin-top: 0; margin-bottom: 0">&nbsp;</td>
-        </tr>
-        <tr>
-            <td width="472">
-                <p style="margin-top: 0; margin-bottom: 0">
-                    <font face="Trebuchet MS" style="font-size: 8pt; font-weight:700" color="#585858">
-                        IT Consulting &amp; Outsourcing</font></td>
-            <td width="504">
-                <p dir="rtl" align="right">
-                    <font face="Tahoma" style="font-size: 8pt" color="#585858">
-									<span lang="fa">&#1582;&#1583;&#1605;&#1575;&#1578; &#1605;&#1588;&#1575;&#1608;&#1585;&#1607; &#1575;&#1740; &#1583;&#1585; &#1586;&#1605;&#1740;&#1606;&#1607;
-									</span>IT <span lang="fa">&#1608; &#1576;&#1585;&#1608;&#1606; &#1587;&#1575;&#1586;&#1605;&#1575;&#1606;&#1740;</span></font>
-            </td>
-        </tr>
-        <tr>
-            <td width="472">
-                <p style="margin-top: 0; margin-bottom: 0">
-                    <font face="Trebuchet MS" style="font-size: 8pt; font-weight:700" color="#585858">Evaluate, Plan,
-                        Deploy, Maintain, Security</font></td>
-            <td width="504">
-                <p dir="rtl">
-                    <font face="Tahoma" style="font-size: 8pt" color="#585858">
-									<span lang="fa">&#1575;&#1585;&#1586;&#1740;&#1575;&#1576;&#1740;&#1548; &#1575;&#1585;&#1575;&#1574;&#1607; &#1606;&#1602;&#1588;&#1607;&#1548; &#1711;&#1587;&#1578;&#1585;&#1588;&#1548;
-									&#1606;&#1711;&#1607;&#1583;&#1575;&#1585;&#1740; &#1608; &#1575;&#1605;&#1606;&#1740;&#1578;</span></font>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <p style="margin-top: 0; margin-bottom: 0">
-                    <font face="Trebuchet MS" style="font-size: 8pt; font-weight:700" color="#585858">Advanced
-                        network and telecom solutions</font></td>
-            <td>
-                <p dir="rtl">
-                    <font face="Tahoma" style="font-size: 8pt" color="#585858">
-									<span id="result_box" class="short_text" lang="fa">
-									<span class>&#1585;&#1575;&#1607; &#1581;&#1604; &#1607;&#1575;&#1740;</span>
-									<span class="hps">&#1662;&#1740;&#1588;&#1585;&#1601;&#1578;&#1607;</span>
-									<span class="hps">&#1583;&#1585; &#1586;&#1605;&#1740;&#1606;&#1607; &#1588;&#1576;&#1705;&#1607; &#1607;&#1575;&#1740; &#1587;&#1575;&#1586;&#1605;&#1575;&#1606;&#1740;
-									&#1608;</span> <span
-                                                class="hps">&#1605;&#1582;&#1575;&#1576;&#1585;&#1575;&#1578;</span></span></font>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <hr size="1" align="right" width="900">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                &nbsp;
-            </td>
-            <td>
-                <p dir="rtl" align="right">
-									<span lang="fa">
-									<font face="Tahoma" style="font-size: 8pt" color="#585858">
-									&#1606;&#1588;&#1575;&#1606;&#1740;: &#1578;&#1607;&#1585;&#1575;&#1606;&#1548; &#1605;&#1601;&#1578;&#1581; &#1588;&#1605;&#1575;&#1604;&#1740;&#1548; &#1606;&#1602;&#1583;&#1740;&#1548; &#1588;&#1605;&#1575;&#1585;&#1607; &#1778;&#1780;&nbsp;
-									&#1578;&#1605;&#1575;&#1587;: &#1776;&#1785;&#1779;&#1781;&#1780;&#1785;&#1776;&#1785;&#1782;&#1779;&#1778;</font></span>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <p align="left" style="margin-top: 0; margin-bottom: 0" dir="rtl">
-			<span style="font-weight: 700">
-			<font face="Trebuchet MS" style="font-size: 8pt" color="#666666">For</font></span><font face="Trebuchet MS"
-                                                                                                    style="font-size: 8pt; font-weight: 700"
-                                                                                                    color="#666666">
-                        more information please contact:
-                        info&nbsp;@ narbon.ir</font></td>
-            <td>
-                <p style="margin-top: 0; margin-bottom: 0" align="right"><b>
-                        <font face="Trebuchet MS" style="font-size: 7pt" color="#666666">
-                            NARBON</font><font style="font-size: 7pt;" face="Trebuchet MS" color="#666666">
-                            technologies © 2003 - 2021, All Rights Reserved</font></b></td>
-        </tr>
-        <tr>
-            <td width="976" colspan="2" align="right">
-                <a href="people/user" class="link">ورود کاربر</a>
-                |
-                <a href="people/support" class="link">ورود پشتیبان</a>
-            </td>
-        </tr>
-    </table>
-</div>
-</body>
+            .org {
+                background: url('pack/WhatsApp Image 2021-08-24 at 10.33.19.jpeg') no-repeat center center fixed;
+                height: 100vh;
+                padding-top: 10%;
+                color: black;
+            }
+            .btn {
+                padding: 5px;
+                width: 8%;
+            }
+            .btn-green {
+                background: green;
+                color: white;
+                border: solid 1px green;
+                border-radius: 5px;
+            }
+            .btn-blue {
+                background: blue;
+                color: white;
+                border: solid 1px blue;
+                border-radius: 5px;
+            }
+        </style>
+        <title>نارتیک</title>
+        <meta charset="utf-8">
+        <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    </head>
+    <body>
+    <div class="body">
+        <div class="org">
+            <h2><i class="fa fa-home"></i></h2>
+            <h2>به نارتیک خوش آمدید</h2>
+<!--            <br>-->
+            <button onclick="user()" class="btn btn-blue"><b>ورود کاربر</b></button>
+            &nbsp;
+            <button onclick="support()" class="btn btn-green"><b>ورود پشتیبان</b></button>
+        </div>
+    </div>
+        <script>
+            function user() {
+                window.location.replace("people/user");
+            }
+
+            function support() {
+                window.location.replace("people/support");
+            }
+        </script>
+    </body>
 </html>
