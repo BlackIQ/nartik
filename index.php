a3db59914ca25d4d17c3055f433d60f129aa7576
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-02 20:23:17 +0330

    Update main template

--- index.php
+++ index.php
@@ -54,171 +54,140 @@ $resc = mysqli_query($connection, $getc);
         <?php
     }
     ?>
-    <div class="text">
-        <span style="font-size: 75px; font-weight: bold; color: white">نارتیک</span>
-        <br>
-        <span style="font-size: 25px; font-weight: bold; color: #fff;">سامانه ثبت درخواست سرویس شرکت ناربن</span>
-        <br>
-        <br>
-        <br>
-        <button type="button" class="btn mbtn" data-bs-toggle="modal" data-bs-target="#loginuser">
-            ورود کاربر
-        </button>
-        <br>
-        <br>
-        <button type="button" class="btn mbtn"" data-bs-toggle="modal"
-                data-bs-target="#create">
-            ساخت حساب کاربری
-        </button>
-        <br>
-        <br>
-        <button type="button" class="btn mbtn" data-bs-toggle="modal" data-bs-target="#loginsupport">
-            ورود پشتیبان
-        </button>
-        <br>
-        <br>
-        <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 half overflow-auto">
+                <div style="display: block;" id="create-user">
+                    <form action="index.php" method="POST">
+                        <h1>ساخت حساب جدید</h1>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label" for="fname">نام</label>
+                                <input type="text" id="fname" name="fname" class="form-control" placeholder="نام">
+                                <br>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label" for="lname">نام خانوادگی</label>
+                                <input type="text" id="lname" name="lname" class="form-control" placeholder="نام خانوادگی">
+                                <br>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label" for="phone">شماره همراه</label>
+                                <input type="text" id="phone" name="phone" class="form-control" placeholder="شماره همراه">
+                                <br>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label" for="email">ایمیل</label>
+                                <input type="text" id="email" name="email" class="form-control" placeholder="ایمیل">
+                                <br>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label" for="username">نام کاربری</label>
+                                <input type="text" id="username" name="username" class="form-control" placeholder="نام کاربری">
+                                <br>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label" for="id">کد ملی</label>
+                                <input type="text" id="id" name="id" class="form-control" placeholder="کد ملی">
+                                <br>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label" for="password">رمز</label>
+                                <input type="password" id="password" name="pass" class="form-control" placeholder="رمز">
+                                <br>
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label" for="conpass">تایید رمز</label>
+                                <input type="password" id="conpass" name="conpass" class="form-control" placeholder="تایید رمز">
+                                <br>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label class="form-label" id="company">انتخاب شرکت</label>
+                                <select id="company" name="company" class="form-control">
+                                    <option selected>انتخاب شرکت</option>
+                                    <?php
+                                    if (mysqli_num_rows($resc) > 0) {
+                                        while ($row = mysqli_fetch_assoc($resc)) {
+                                            ?>
+                                            <option class="text-primary"
+                                                    value="<?php echo $row["id"]; ?>"><?php echo $row["id"]; ?></option>
+                                            <?php
+                                        }
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <br>
+                        <div>
+                            <button type="submit" name="create_user" class="btn xbtn">ساخت حساب</button>
+                        </div>
+                        <hr>
+                        <div class="hints">
+                            <a onclick="return show('login-user', 'create-user');" class="nlink">ورود به عنوان کاربر</a>
+                            <br>
+                            <a onclick="return show('login-support', 'create-user');" class="nlink">ساخت حساب پشتیبان</a>
+                        </div>
+                    </form>
+                </div>
+                <div style="display: none;" id="login-user">
+                    <form action="index.php" method="POST">
+                        <h1>ورود کاربران</h1>
+                        <br>
+                        <label class="form-label" for="id">کد ملی</label>
+                        <input type="text" id="id" name="id" class="form-control" placeholder="کد ملی">
+                        <br>
+                        <label class="form-label" for="password">رمز حساب</label>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="رمز حساب">
+                        <br>
+                        <div>
+                            <button type="submit" name="login_user" class="btn xbtn">ورود</button>
+                        </div>
+                        <hr>
+                        <div class="hints">
+                            <a onclick="return show('login-support', 'login-user');" class="nlink">ورود به عنوان پشتیبان</a>
+                            <br>
+                            <a onclick="return show('create-user', 'login-support');" class="nlink">ساخت حساب کاربری</a>
+                        </div>
+                    </form>
+                </div>
+                <div style="display: none;" id="login-support">
+                    <form action="index.php" method="POST">
+                        <h1>ورود پشتیبان</h1>
+                        <br>
+                        <label class="form-label" for="username">نام کاربری</label>
+                        <input type="text" name="username" id="username" class="form-control" placeholder="نام کاربری">
+                        <br>
+                        <label class="form-label" for="password">رمز حساب</label>
+                        <input type="password" name="password" id="password" class="form-control" placeholder="رمز ورود">
+                        <br>
+                        <div>
+                            <button type="submit" name="login_support" class="btn xbtn">ورود</button>
+                        </div>
+                        <hr>
+                        <div class="hints">
+                            <a onclick="return show('login-user', 'login-support');" class="nlink">ورود به عنوان کاربر</a>
+                            <br>
+                            <a onclick="return show('create-user', 'login-support');" class="nlink">ساخت حساب کاربری</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </body>
+<script src="pack/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
         crossorigin="anonymous"></script>
 </html>
-
-<form method="post" action="index.php">
-    <div class="modal fade" id="loginuser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">ورود کاربر</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body center">
-                    <label class="form-label" for="id">کد ملی</label>
-                    <input type="text" id="id" name="id" class="form-control border-primary text-primary"
-                           placeholder="کد ملی">
-                    <br>
-                    <label class="form-label" for="password">رمز حساب</label>
-                    <input type="password" id="password" name="password"
-                           class="form-control border-primary text-primary"
-                           placeholder="رمز حساب">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">خروج</button>
-                    <button type="submit" name="login_user" class="btn btn-primary">ورود</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
-
-<form method="post" action="index.php">
-    <div class="modal fade" id="loginsupport" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">ورود پشتیبان</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body center">
-                    <label class="form-label" for="username">نام کاربری</label>
-                    <input type="text" name="username" id="username" class="form-control border-success text-success" placeholder="نام کاربری">
-                    <br>
-                    <label class="form-label" for="password">رمز حساب</label>
-                    <input type="password" name="password" id="password" class="form-control border-success text-success"
-                           placeholder="رمز ورود">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">خروج</button>
-                    <button type="submit" name="login_support" class="btn btn-success text-white">ورود</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
-
-<form method="post" action="index.php">
-    <div class="modal fade" id="create" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">ساخت حساب کاربری جدید</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body center">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="form-label" for="fname">نام</label>
-                            <input type="text" id="fname" name="fname" class="form-control text-primary border-primary" placeholder="نام">
-                            <br>
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="lname">نام خانوادگی</label>
-                            <input type="text" id="lname" name="lname" class="form-control text-primary border-primary" placeholder="نام خانوادگی">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="form-label" for="phone">شماره همراه</label>
-                            <input type="text" id="phone" name="phone" class="form-control text-primary border-primary" placeholder="شماره همراه">
-                            <br>
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="email">ایمیل</label>
-                            <input type="text" id="email" name="email" class="form-control text-primary border-primary" placeholder="ایمیل">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="form-label" for="username">نام کاربری</label>
-                            <input type="text" id="username" name="username" class="form-control text-primary border-primary" placeholder="نام کاربری">
-                            <br>
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="id">کد ملی</label>
-                            <input type="text" id="id" name="id" class="form-control text-primary border-primary" placeholder="کد ملی">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="form-label" for="password">رمز</label>
-                            <input type="password" id="password" name="pass" class="form-control text-primary border-primary" placeholder="رمز">
-                            <br>
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label" for="conpass">تایید رمز</label>
-                            <input type="password" id="conpass" name="conpass" class="form-control text-primary border-primary" placeholder="تایید رمز">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <label class="form-label" id="company">انتخاب شرکت</label>
-                            <select id="company" name="company" class="form-control border-primary text-primary">
-                                <option selected>انتخاب شرکت</option>
-                                <?php
-                                if (mysqli_num_rows($resc) > 0) {
-                                    while ($row = mysqli_fetch_assoc($resc)) {
-                                        ?>
-                                        <option class="text-primary"
-                                                value="<?php echo $row["id"]; ?>"><?php echo $row["id"]; ?></option>
-                                        <?php
-                                    }
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">خروج</button>
-                    <button type="submit" name="create_user" class="btn btn-primary text-white" style="color: white">ساخت حساب</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
