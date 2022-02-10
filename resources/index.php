f1a8887850dd52bd8bc39b1a18594ec2ca9980b4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-02-11 00:14:42 +0330

    Add redirect

--- resources/index.php
+++ resources/index.php
@@ -1,19 +1,4 @@
 <?php
 
-session_start();
-
-if ($_SESSION['status'] == true) {
-    $who = $_SESSION['who'];
-    ?>
-    <script>
-        window.location.replace("../people/<?php echo $who; ?>");
-    </script>
-    <?php
-}
-else {
-    ?>
-    <script>
-        window.location.replace("../");
-    </script>
-    <?php
-}
+include('config/config.php');
+header('location:' . $path);
